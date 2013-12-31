72c47cdd51bab3f0e80f05edff67d5b7961da373
Curtis Larson <quackware@gmail.com>
2013-12-31 13:02:10 -0600

    Worked on reverse linked list a bit

---
+++ src/Chapter3/ReverseLinkedList.java
@@ -0,0 +1,68 @@
+public class ReverseLinkedList
+{
+	public static void main(String[] args)
+	{
+		ReverseLinkedList ll = new ReverseLinkedList();
+		ll.run();
+	}
+
+	public void run()
+	{
+		Node head = new Node(1);
+		Node second = new Node(2);
+		Node third = new Node(3);
+		Node fourth = new Node(4);
+		head.setNext(second);
+		second.setNext(third);
+		third.setNext(fourth);
+
+		Node newHead = reverse(head, head.getNext());
+
+		while (newHead.getNext() != null) {
+			System.out.println(newHead.getData());
+			newHead = newHead.getNext();
+		}
+	}
+
+	// 1 -> 2 -> 3 -> 4
+	// 4 -> 3 -> 2 -> 1
+	// Recursive until 
+	public static Node reverse(Node node, Node nextNode)
+	{
+		if (nextNode == null) {
+			return node;
+		}
+		else {
+			Node newNode = reverse(nextNode, nextNode.getNext());
+			nextNode.setNext(node);
+			return nextNode;
+		}
+	}
+
+
+	private class Node
+	{
+		Node mNext;
+		int mData;
+
+		Node(int data)
+		{
+			mData = data;
+		}
+
+		void setNext(Node next)
+		{
+			mNext = next;
+		}
+
+		Node getNext()
+		{
+			return mNext;
+		}
+
+		int getData()
+		{
+			return mData;
+		}
+	}
+}
