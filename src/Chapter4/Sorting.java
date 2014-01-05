a540554f98cc856babf832601f15416e6d0ed55c
Curtis Larson <quackware@gmail.com>
2014-01-04 18:54:40 -0800

    Implemented quicksort

--- src/Chapter4/Sorting.java
+++ src/Chapter4/Sorting.java
@@ -9,11 +9,18 @@ public class Sorting
 		}
 		System.out.println();
 
-		int[] sorted = selectionSort(arrayToSort);
+		quickSort(arrayToSort);
+
+		for (int i = 0;i < arrayToSort.length; i++) {
+			System.out.print(arrayToSort[i] + " ");
+		}
+		System.out.println();
+
+		/*int[] sorted = selectionSort(arrayToSort);
 		for (int i = 0;i < sorted.length; i++) {
 			System.out.print(sorted[i] + " ");
 		}
-		System.out.println();
+		System.out.println();*/
 	}
 
 	public static int[] selectionSort(int[] input)
@@ -33,5 +40,40 @@ public class Sorting
 			input[minIndex] = Integer.MAX_VALUE;
 		}
 		return returnArray;
+	}
+
+	public static void quickSort(int[] input)
+	{
+		quickSortHelper(0, input.length - 1, input);
+	}
+
+	public static void quickSortHelper(int low, int high, int[] input)
+	{
+		int i = low;
+		int j = high;
+		int pivot = input[low + (high - low)/2];
+		while (i <= j) {
+			while (input[i] < pivot) {
+				i++;
+			}
+			while (input[j] > pivot) {
+				j--;
+			}
+
+			if (i <= j) {
+				int temp = input[i];
+				input[i] = input[j];
+				input[j] = temp;
+				i++;
+				j--;
+			}
+
+			if (low < j) {
+				quickSortHelper(low, j, input);
+			}
+			if (high > i) {
+				quickSortHelper(i, high, input);
+			}
+		}
 	}
 }
